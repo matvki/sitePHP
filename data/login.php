c1be9b3bd867c121b75ca56c482c578313b6e181
admin <admin@iMac3.local>
2020-02-18 10:11:28 +0100

    last form

--- data/login.php
+++ data/login.php
@@ -1,26 +1,30 @@
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <link rel="icon" type="image/png" href="./img/favicon.png" />
     <meta charset="UTF-8">
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/header.css">
-    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/login.css">
     <title>TAF1 | Accueil</title>
 </head>
-
 <body>
     <?php include_once './templates/header.php' ?>
-    <form action="./controllers/login.php" method="$_POST">
-        <h1>Connexion</h1>
-        <label for="name">Votre nom : </label>
-        <input type="text" name="name" id="name" value="jon">
-        <label for="password">Votre mot de passe : </label>
-        <input type="password" name="password" id="password" value="1234">
-        <input type="submit" value="Validé">
-    </form>
-
+    <div id="alignCenter" class="flex">
+        <form action="./controllers/login.php" method="$_POST">
+            <h1>Connexion</h1>
+            <div id="inputForm" class="flex">
+                <div class="inputLabel">
+                    <label for="name">Votre nom : </label>
+                    <input type="text" name="name" id="name" value="jon">
+                </div>
+                <div class="inputLabel">
+                    <label for="password">Votre mot de passe : </label>
+                    <input type="password" name="password" id="password" value="1234">
+                </div>
+                <input id="submit" type="submit" value="Validé">
+            </div>
+        </form>
+    </div>
 </body>
-
 </html>
