f24a3265c7018837d90745b7b9139a4933731c94
admin <admin@iMac3.local>
2020-02-18 11:16:46 +0100

    end header

--- data/templates/header.php
+++ data/templates/header.php
@@ -3,8 +3,16 @@
     <ul class="flex">
         <li><a href="/">Accueil</a></li>
         <li><a href="#">Information</a></li>
-        <li><a href="./login.php">Connexion</a></li>
-        <li><a href="./controllers/delog.php">Déconnection</a></li>
-        <li><a href="#">Paramètre</a></li>
+        <?php if (isset($_SESSION['userName'])) : ?>
+            <li id="scroll">
+                <span><?= $_SESSION['userName'] ?></span>
+                <ul>
+                    <li><a href="#">Paramètre</a></li>
+                    <li><a href="./controllers/delog.php">Déconnection</a></li>
+                </ul>
+            </li>
+        <?php else : ?>
+            <li><a href="./login.php">Connexion</a></li>
+        <?php endif ?>
     </ul>
 </header>
