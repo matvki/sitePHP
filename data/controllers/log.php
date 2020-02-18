b84b3dd972412a934eb961a43101f11e7567dbf3
admin <admin@iMac3.local>
2020-02-18 17:15:54 +0100

    update log.php

--- data/controllers/log.php
+++ data/controllers/log.php
@@ -3,7 +3,7 @@ session_start();
 $_SESSION['name'] = $_POST['name'];
 $_SESSION['password'] = $_POST['password'];
 if (($_SESSION['name'] == 'Jon' || $_SESSION['name'] == 'jon' || $_SESSION['name'] == 'JOn' || $_SESSION['name'] == 'JON' || $_SESSION['name'] == 'jOn' || $_SESSION['name'] == 'jON' || $_SESSION['name'] == 'joN') && ($_SESSION['password'] == '1234')) {
-    $_SESSION['userName'] = $_SESSION['name'];
+    $_SESSION['userName'] = 'Jon';
     $_SESSION['one']['picture'] = 'yes';
     $_SESSION['one']['text'] = 'yes';
     $_SESSION['two']['picture'] = 'yes';
