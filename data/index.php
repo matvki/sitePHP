2d2e44ad974b73007911f8c7d32e5058b5cfd5a4
admin <admin@iMac3.local>
2020-02-18 09:36:08 +0100

    add first header

---
+++ data/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="./css/style.css">
+  <link rel="stylesheet" href="./css/header.css">
+  <link rel="stylesheet" href="./css/index.css">
+  <title>TAF1 | Accueil</title>
+</head>
+<body>
+  <?php include_once './templates/header.php' ?>
+  
+</body>
+</html>
---
+++ data/templates/header.php
@@ -0,0 +1,10 @@
+<?php session_start() ?>
+<header>
+    <ul class="flex">
+        <li><a href="/">Accueil</a></li>
+        <li><a href="#">Information</a></li>
+        <li><a href="#">Connexion</a></li>
+        <li><a href="#">Déconnection</a></li>
+        <li><a href="#">Paramètre</a></li>
+    </ul>
+</header>
